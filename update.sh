a2c065ea844720801e3c794be6d5f0f119df4a54
aNz <107313945+anzclan@users.noreply.github.com>
2023-02-01 20:51:25 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo -e "$BLUE│$NC  $INFO Download Update Data.."
-sleep 1
+export DOTY="https://source.dotycat.com"
 echo -e "$BLUE│$NC  $INFO Remove Old Data"
+sleep 1
+echo -e "$BLUE│$NC  $INFO Download Update Data.."
+wget -q -O /usr/local/sbin/xxx "$DOTY/menu.sh" && chmod +x /usr/local/sbin/xxx
 sleep 1
 echo -e "$BLUE│$NC  $INFO Cecking Update"
 sleep 1
