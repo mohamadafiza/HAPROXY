ce0153952746fc3d30fd206a58ed3409ae4b1772
aNz <107313945+anzclan@users.noreply.github.com>
2023-02-01 20:40:22 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "$BLUE│$NC  $INFO Download Update Data.."
+sleep 1
+echo -e "$BLUE│$NC  $INFO Remove Old Data"
+sleep 1
+echo -e "$BLUE│$NC  $INFO Cecking Update"
+sleep 1
+echo -e "$BLUE│$NC  $INFO Update Successfully"
+sleep 1
