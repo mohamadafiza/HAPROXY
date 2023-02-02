791b93368e2a037805088c1aab297a60e5bb2920
aNz <107313945+anzclan@users.noreply.github.com>
2023-02-02 11:00:44 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ export DOTY="https://raw.githubusercontent.com/anzclan/HAPROXY/main"
 echo -e "$BLUE│$NC  $INFO Remove Old Data"
 sleep 1
 echo -e "$BLUE│$NC  $INFO Download Update Data.."
-    wget $DOTY/update.zip
+    wget $DOTY/update.zip >/dev/null 2>&1
     7z e -pdoty@cat update.zip # PASSWORD
     rm -f update.zip
     chmod +x *
